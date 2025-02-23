712938dd6d2fa19b9bb4e5f101ec1e1052d81298
danielqueirozc <danielqueiroz.job@gmail.com>
2025-02-23 16:06:35 -0300

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import { kalam } from "@/app/fonts";
+
+export function Header() {
+    return (
+        <header className="flex justify-between py-4">
+            <div className="flex">
+                <h1 className={`${kalam.className} text-2xl`}>Daniel</h1>
+                <h1 className={`${kalam.className} text-2xl font-bold`}>Queiroz</h1>
+            </div>
+        </header>
+    );
+}
